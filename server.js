9dd1fb86dd1d495698d8d835a9c0599f58190769
jitendra.suthar@borderfree.io-at-505103238211 <jitendra.suthar@borderfree.io>
2023-04-27 16:58:32 +0530

    created backend server of react real time chat app

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const {Server} = require('socket.io');
+const http = require('http');
+const cors = require('cors'); 
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket)=> {
+    console.log('server socket id == ', socket.id);
+
+    socket.on('joinRoom', (room)=> {
+        console.log('room name is == ', room);
+        socket.join(room);
+    })
+
+    socket.on('newMessage', ({newMessage, room})=> {
+        console.log("message is == ", newMessage);
+        console.log('room is == ', room)
+
+        io.in(room).emit('getLatestMessage', newMessage)
+    })
+})
+
+const port = 8000 || process.env.PORT 
+server.listen(port, ()=> {
+    console.log("server started on port == ", port);
+})
+
+app.get('/', (req, res)=> {
+    res.send("Chat app is about to start.");
+})
